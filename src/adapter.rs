bab56d46b5a9ddd1f266b578dc3e9656be165a0a
James Hobin <hobinjk@gmail.com>
2017-12-15 17:55:53 -0600

    Types!

--- src/adapter.rs
+++ src/adapter.rs
@@ -1,7 +1,8 @@
 use nanomsg::{Protocol, Socket};
-use serde_json;
+use serde_json::{self, Map, Value};
 use std::io::{self, Read, Write};
 
+const BASE_URL: &'static str = "ipc:///tmp";
 const ADAPTER_MANAGER_URL: &'static str = "ipc:///tmp/gateway.addonManager";
 
 struct RegisterPlugin {
@@ -25,6 +26,118 @@ impl RegisterPlugin {
     }
 }
 
+#[derive(Deserialize)]
+#[serde(tag = "messageType", content = "data", rename_all="camelCase")]
+enum GatewayPluginMessage {
+    #[serde(rename_all = "camelCase")]
+    RegisterPluginReply {
+        plugin_id: String,
+        ipc_base_addr: String,
+    },
+    #[serde(rename_all = "camelCase")]
+    UnloadPlugin {
+        plugin_id: String,
+    },
+    #[serde(rename_all = "camelCase")]
+    UnloadAdapter {
+        plugin_id: String,
+        adapter_id: String,
+    },
+}
+
+#[derive(Deserialize)]
+#[serde(tag = "messageType", content = "data", rename_all="camelCase")]
+enum GatewayAdapterMessage {
+    #[serde(rename_all = "camelCase")]
+    SetProperty {
+        plugin_id: String,
+        adapter_id: String,
+        device_id: String,
+        property: Property,
+    },
+    #[serde(rename_all = "camelCase")]
+    StartPairing {
+        plugin_id: String,
+        adapter_id: String,
+        timeout: f64,
+    },
+    #[serde(rename_all = "camelCase")]
+    CancelPairing {
+        plugin_id: String,
+        adapter_id: String,
+    },
+    #[serde(rename_all = "camelCase")]
+    RemoveThing {
+        plugin_id: String,
+        adapter_id: String,
+        device_id: String,
+    },
+    #[serde(rename_all = "camelCase")]
+    CancelRemoveThing {
+        plugin_id: String,
+        adapter_id: String,
+        device_id: String,
+    },
+}
+
+#[derive(Serialize)]
+#[serde(tag = "messageType", content = "data", rename_all="camelCase")]
+enum PluginGatewayMessage {
+    #[serde(rename_all = "camelCase")]
+    RegisterPlugin {
+        plugin_id: String,
+    },
+    #[serde(rename_all = "camelCase")]
+    PluginUnloaded {
+        plugin_id: String,
+    },
+    #[serde(rename_all = "camelCase")]
+    AdapterUnloaded {
+        plugin_id: String,
+        adapter_id: String,
+    },
+}
+
+#[derive(Serialize)]
+#[serde(tag = "messageType", content = "data", rename_all="camelCase")]
+enum AdapterGatewayMessage {
+    #[serde(rename_all = "camelCase")]
+    AddAdapter {
+        plugin_id: String,
+        adapter_id: String,
+        name: String,
+    },
+    #[serde(rename_all = "camelCase")]
+    HandleDeviceAdded {
+        plugin_id: String,
+        adapter_id: String,
+        id: String,
+        name: String,
+        typ: String,
+        properties: Map<String, Value>,
+        actions: Map<String, Value>,
+    },
+    #[serde(rename_all = "camelCase")]
+    HandleDeviceRemoved {
+        plugin_id: String,
+        adapter_id: String,
+        id: String,
+    },
+    #[serde(rename_all = "camelCase")]
+    PropertyChanged {
+        plugin_id: String,
+        adapter_id: String,
+        device_id: String,
+        property: Property,
+    },
+}
+
+#[derive(Deserialize, Serialize)]
+struct Property {
+    name: String,
+    value: f64,
+}
+
 pub fn register_plugin(id: &str) -> Result<(), io::Error> {
     let mut socket = Socket::new(Protocol::Req)?;
     let mut endpoint = socket.connect(ADAPTER_MANAGER_URL)?;
@@ -33,6 +146,7 @@ pub fn register_plugin(id: &str) -> Result<(), io::Error> {
     let mut rep = String::new();
     socket.read_to_string(&mut rep)?;
     println!("We got it! {}", rep);
+    let msg: GatewayPluginMessage = serde_json::from_str(&rep)?;
     // open a Req channel to adapterManager
     // send {messageType: 'registerPlugin', data: { pluginId: id }}
     // receives
@@ -46,6 +160,26 @@ pub fn register_plugin(id: &str) -> Result<(), io::Error> {
     // connect to ipcBaseAddr as pair
     // then handle everything
 
+    let ipc_base_addr = match msg {
+        GatewayPluginMessage::RegisterPluginReply {ipc_base_addr, ..} => {
+            ipc_base_addr
+        },
+        _ => {
+            return Err(io::Error::new(io::ErrorKind::Other, format!("gateway sent unexpected message {}", rep)));
+        }
+    };
+
+    let mut socket_pair = Socket::new(Protocol::Pair)?;
+    let mut endpoint_pair = socket_pair.connect(&format!("{}/{}", BASE_URL, &ipc_base_addr))?;
+
+    // wee woo wee woo
+    for i in 1..10 {
+        socket_pair.read_to_string(&mut rep)?;
+        println!("we got another {}", rep);
+    }
+
     endpoint.shutdown()?;
+    endpoint_pair.shutdown()?;
+
     Ok(())
 }
